606c6b9bb7f7e8082b4d7dbb89fcd237dbe31c31
Commit1 <zlatanovic.basketball@gmail.com>
2023-10-19 18:19:46 +0200

    Pocetak projekta

---
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/SwingGui.java
@@ -0,0 +1,12 @@
+package raf.dsw.classycraft.app.gui.swing;
+
+import raf.dsw.classycraft.app.core.Gui;
+import raf.dsw.classycraft.app.gui.swing.view.MainFrame;
+
+public class SwingGui implements Gui {
+    private MainFrame mainFrame;
+    public void start() {
+        mainFrame = MainFrame.getInstance();
+        mainFrame.setVisible(true);
+    }
+}
---
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/view/MainFrame.java
@@ -0,0 +1,36 @@
+package raf.dsw.classycraft.app.gui.swing.view;
+
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private static MainFrame insance;
+    //buduca polja za sve komponente view-a na glavnom prozoru
+    private MainFrame(){
+
+    }
+    private void initialize(){
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        setSize(screenWidth / 2, screenHeight / 2);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("ClassyCrafT");
+
+        MyMenuBar menu = new MyMenuBar();
+        setJMenuBar(menu);
+
+        MyToolBar toolBar = new MyToolBar();
+        add(toolBar, BorderLayout.NORTH);
+    }
+    public static MainFrame getInstance(){
+        if(insance == null){
+            insance = new MainFrame();
+            insance.initialize();
+        }
+        return insance;
+    }
+}
