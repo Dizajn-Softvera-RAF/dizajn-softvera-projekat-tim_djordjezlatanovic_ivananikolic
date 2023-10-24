305ca63b18f7bdca63afe387ce8ce718b283f1a2
Commit1 <zlatanovic.basketball@gmail.com>
2023-10-24 12:39:50 +0200

    PrvoPredavanje, ubacivanje edita i about us opcije #2

---
+++ src/main/java/raf/dsw/classycraft/app/controller/AboutUs.java
@@ -0,0 +1,29 @@
+package raf.dsw.classycraft.app.controller;
+
+
+
+import raf.dsw.classycraft.app.gui.swing.view.MyMenuBar;
+import raf.dsw.classycraft.app.gui.swing.view.MyToolBar;
+import raf.dsw.classycraft.app.gui.swing.view.ScenaZaAboutUs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+public class AboutUs extends AbstractClassyAction{
+    private ScenaZaAboutUs scenaZaAboutUs;
+    public AboutUs(){
+        putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_F1, ActionEvent.ALT_MASK));
+        putValue(SMALL_ICON, loadIcon("/images/AboutUs.png"));
+        putValue(NAME, "About us");
+        putValue(SHORT_DESCRIPTION, "About us");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        ScenaZaAboutUs sc = new ScenaZaAboutUs();
+        sc.setVisible(true);
+
+    }
+}
