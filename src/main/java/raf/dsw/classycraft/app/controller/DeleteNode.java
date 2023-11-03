2917d43f5673869d40eb6a2581fc6b364ee588e1
Commit1 <zlatanovic.basketball@gmail.com>
2023-11-03 16:45:34 +0100

    Promene vezane za pucanje programa i izbacivanje message-a

--- src/main/java/raf/dsw/classycraft/app/controller/DeleteNode.java
+++ src/main/java/raf/dsw/classycraft/app/controller/DeleteNode.java
@@ -1,11 +1,17 @@
 package raf.dsw.classycraft.app.controller;
 
 import raf.dsw.classycraft.app.JTree.model.ClassyTreeItem;
+import raf.dsw.classycraft.app.composite.abstraction.ClassyNodeComposite;
+import raf.dsw.classycraft.app.composite.implementation.Package;
+import raf.dsw.classycraft.app.composite.implementation.ProjectExplorer;
+import raf.dsw.classycraft.app.core.ApplicationFramework;
 import raf.dsw.classycraft.app.gui.swing.view.MainFrame;
+import raf.dsw.classycraft.app.observer.message.MessageType;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.time.LocalDateTime;
 
 public class DeleteNode extends AbstractClassyAction{
     public DeleteNode() {
@@ -18,7 +24,23 @@ public class DeleteNode extends AbstractClassyAction{
     @Override
     public void actionPerformed(ActionEvent e) {
         ClassyTreeItem selected = MainFrame.getInstance().getCLassyTree().getSelectedNode();
-        MainFrame.getInstance().getCLassyTree().deleteChild(selected);
+        ClassyNodeComposite node = null;
+        if(selected == null){
+            ApplicationFramework.getInstance().getMessageGenerator().generateMessage("Morate selektovati cvor u stablu koji zelite da obrisete", MessageType.WARNING, LocalDateTime.now());
+        }
+        else if(selected != null){
+            node = (ClassyNodeComposite) selected.getClassyNode();
+            if (node instanceof ProjectExplorer) {
+                ApplicationFramework.getInstance().getMessageGenerator().generateMessage("Project Explorer ne moze da se obrise", MessageType.WARNING, LocalDateTime.now());
+            }
+            else if(selected instanceof ClassyTreeItem){
+
+                MainFrame.getInstance().getCLassyTree().deleteChild(selected);
+
+            }
+        }
 
     }
-}
+
+    }
+
