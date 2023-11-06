2df20cd9aa0ca8df1279e56f0cb55a86a28117cd
Ivana Nikolic <icanikolic085@gmail.com>
2023-11-06 17:11:38 +0100

    brisanje i dodavanje tabova

--- src/main/java/raf/dsw/classycraft/app/observer/Publisher.java
+++ src/main/java/raf/dsw/classycraft/app/observer/Publisher.java
@@ -3,5 +3,5 @@ package raf.dsw.classycraft.app.observer;
 public interface Publisher {
     void addSubscriber(Subscriber var1);
     void removeSubscriber(Subscriber var1);
-    void notifySubscriber(Object var1);
+    void notifySubscriber(Object var1, String tekst);
 }
